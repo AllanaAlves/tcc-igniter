0c357d19cfbc5b71e5aab79868126e5595a4b843
costinha7 <rogercostinha73@gmail.com>
2022-05-05 20:12:01 -0300

    arquivo home.php criado

---
+++ app/Views/home.php
@@ -0,0 +1,41 @@
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script>
+
+
+    google.charts.load('current', {'packages':['corechart']});
+
+    function desenharPizza (){
+
+        var tabela = new google.visualization.DataTable();
+        tabela.addColumn('string','categorias');
+        tabela.addColumn('number','valores');
+        tabela.addRows([
+
+            ['Tipo1',2000],
+            ['Tipo2',500],
+            ['Tipo3',230],
+            ['Tipo4',50],
+            ['Tipo5 ',900],
+            ['Tipo6',260]
+        ]);
+        var opcoes ={
+            'title':'Titulo Grafico',
+            'heigth': 500, 
+            'width': 600,
+            'pieHole':0.3,
+            is3D : true,
+        };
+
+        var grafico = new google.visualization.PieChart(document.getElementById('graficoPizza'));
+        grafico.draw(tabela, opcoes);
+}
+
+google.charts.setOnLoadCallback(desenharPizza);
+
+
+</script>
+
+
+<div id="graficoPizza"></div>
