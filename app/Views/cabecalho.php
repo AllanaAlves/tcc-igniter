50c007f770086087c3f631285c0b88cd1314a1a0
lucasmenchon <it.lucas@outlook.com>
2022-05-10 00:17:19 -0300

    conferencia e organizacao no codigo, necessario correcao de bugs.

--- app/Views/cabecalho.php
+++ app/Views/cabecalho.php
@@ -5,7 +5,6 @@ if( !isset($_SESSION['id_funcionario']) OR !isset($_SESSION['usuario_funcionario
     header("Location: index");
     
 }*/
-
 ?>
 
 <!DOCTYPE html>
@@ -22,4 +21,4 @@ if( !isset($_SESSION['id_funcionario']) OR !isset($_SESSION['usuario_funcionario
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <title class="titlesite">Book Solution</title>
 </head>
-
+<body>
